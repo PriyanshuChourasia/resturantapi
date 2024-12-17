ef2be52b7c507d7c4f9153c4e107ef19f06a6614
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2024-12-17 12:07:41 +0530

    Auth controller created and api route test and now api working true

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::get('/restaurant', function (Request $request) {
+    return response()->json([
+        'data' => [
+            'name' => 'Priyanshu Chourasia',
+            'email' => 'priyanshuchourasia916@gmail.com',
+            'occupation' => 'Software Developer'
+        ],
+        'status' => true,
+    ], 200);
+});
+
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+});
